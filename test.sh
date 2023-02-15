e7b5eb1d6246b6c31437c0a0c85f16625bdefbfc
Julien Cretin <cretin@google.com>
2023-02-15 13:55:32 +0100

    Add simple CI wrapper around test.sh

--- test.sh
+++ test.sh
@@ -15,7 +15,12 @@
 
 set -e
 
-if [[ ! -e examples/assemblyscript/node_modules ]]; then
+if [ ! -e third_party/WebAssembly/spec/.git ]; then
+  ( set -x
+    git submodule update --init
+  )
+fi
+if [ ! -e examples/assemblyscript/node_modules ]; then
   ( set -x
     cd examples/assemblyscript
     npm install --no-save assemblyscript
