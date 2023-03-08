d8632b1d428f299f9c331bce2fcdf1870fcc16a9
Julien Cretin <cretin@google.com>
2023-03-08 16:52:56 +0100

    Use change detection for build.rs

--- crates/runner-nordic/build.rs
+++ crates/runner-nordic/build.rs
@@ -21,4 +21,6 @@ fn main() {
     let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
     File::create(out.join("memory.x")).unwrap().write_all(include_bytes!("memory.x")).unwrap();
     println!("cargo:rustc-link-search={}", out.display());
+    println!("cargo:rerun-if-changed=memory.x");
+    println!("cargo:rerun-if-changed=build.rs");
 }
