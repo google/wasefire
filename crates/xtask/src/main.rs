google/wasefire

commit e5cf70ccaa0bf27038beeca33399e25d17e912c1

Add exercises for the Rust prelude to the book (#270)

crates/xtask/src/main.rs
@@ -264,17 +264,20 @@ impl AppletOptions {
     }
 
     fn execute_rust(&self, main: &MainOptions) -> Result<()> {
-        let (dir, wasm) = if self.name.starts_with(['.', '/']) {
-            let dir = &self.name;
+        let dir = if self.name.starts_with(['.', '/']) {
+            self.name.clone()
+        } else {
+            format!("examples/{}/{}", self.lang, self.name)
+        };
+        ensure!(Path::new(&dir).exists(), "{dir} does not exist");
+        let wasm = {
             // We could use `cargo metadata --no-deps --format-version=1` and parse the JSON to get
             // both the target name and target directory.
             let mut sed = Command::new("sed");
             sed.args(["-n", r#"s/^name = "\(.*\)"$/\1/p"#, "Cargo.toml"]);
-            sed.current_dir(dir);
-            let name = read_output_line(&mut sed)?;
-            (dir.to_string(), format!("{dir}/{}", wasm_target(&name)))
-        } else {
-            (format!("examples/{}/{}", self.lang, self.name), wasm_target(&self.name))
+            sed.current_dir(&dir);
+            let name = read_output_line(&mut sed)?.replace('-', "_");
+            format!("target/wasm32-unknown-unknown/release/{name}.wasm")
         };
         let mut cargo = Command::new("cargo");
         let mut rustflags = vec![
@@ -578,10 +581,6 @@ impl RunnerOptions {
     fn board_target(&self) -> String {
         format!("target/{}/release/runner-{}", self.target(), self.name)
     }
-}
-
-fn wasm_target(name: &str) -> String {
-    format!("target/wasm32-unknown-unknown/release/{name}.wasm")
 }
 
 fn execute_command(command: &mut Command) -> Result<()> {

crates/runner-host/src/main.rs
@@ -75,8 +75,10 @@ async fn main() -> Result<()> {
     #[cfg(feature = "usb")]
     board::usb::Usb::init()?;
     #[cfg(not(feature = "web"))]
-    tokio::spawn(async move {
+    tokio::task::spawn_blocking(|| {
         use std::io::BufRead;
+        // The tokio::io::Stdin documentation recommends to use blocking IO in a dedicated thread.
+        // Note that because of this, the runtime may not exit until the user press enter.
         for line in std::io::stdin().lock().lines() {
             let pressed = match line.unwrap().as_str() {
                 "button" => None,

examples/rust/exercises/client/Cargo.toml
@@ -1,0 +1,16 @@
+[package]
+name = "client"
+version = "0.1.0"
+authors = ["Julien Cretin <cretin@google.com>"]
+license = "Apache-2.0"
+edition = "2021"
+
+[dependencies]
+clap = { version = "4.4.4", features = ["derive"] }
+interface = { path = "../interface" }
+p256 = "0.13.2"
+rand = "0.8.5"
+serialport = { version = "4.2.2", optional = true }
+
+[features]
+usb = ["dep:serialport"] # Use USB serial instead of UART.

examples/rust/exercises/client/src/main.rs
@@ -1,0 +1,123 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::io::{ErrorKind, Read, Write};
+use std::time::Duration;
+
+use clap::Parser;
+use interface::{Request, Response};
+use p256::ecdsa::signature::hazmat::PrehashVerifier;
+use p256::ecdsa::{Signature, VerifyingKey};
+use rand::RngCore;
+
+#[derive(Parser)]
+enum Command {
+    /// Sends a Register request and stores the public key.
+    Register {
+        /// The private key name.
+        name: String,
+    },
+
+    /// Sends an Authenticate request (with a random challenge) and verifies the signature.
+    Authenticate {
+        /// The private key name.
+        name: String,
+    },
+
+    /// Sends a List request and prints the result.
+    List,
+
+    /// Sends a Delete request and deletes the public key.
+    Delete {
+        /// The private key name.
+        name: String,
+    },
+}
+
+fn main() {
+    let request = match Command::parse() {
+        Command::Register { name } => Request::Register { name },
+        Command::Authenticate { name } => {
+            let mut challenge = [0; 32];
+            rand::thread_rng().fill_bytes(&mut challenge);
+            Request::Authenticate { name, challenge }
+        }
+        Command::List => Request::List,
+        Command::Delete { name } => Request::Delete { name },
+    };
+    let mut serial = connect();
+    eprintln!("Sending {request:02x?}.");
+    serial.write_all(&interface::serialize(&request)).unwrap();
+    let response = interface::deserialize::<Result<Response, String>>(|x| match serial.read(x) {
+        Err(e) if e.kind() == ErrorKind::WouldBlock => {
+            panic!("Device did not reply. Is it running?");
+        }
+        x => x.unwrap(),
+    });
+    eprintln!("Received {response:02x?}.");
+    let response = match response {
+        Ok(x) => x,
+        Err(reason) => {
+            println!("Error: {reason}");
+            return;
+        }
+    };
+    match (request, response) {
+        (Request::Register { name }, Response::Register { x, y }) => {
+            let mut public = x.to_vec();
+            public.extend_from_slice(&y);
+            std::fs::write(format!("{name}.pub"), public).unwrap();
+        }
+        (Request::Authenticate { name, challenge }, Response::Authenticate { r, s }) => {
+            let mut public = std::fs::read(format!("{name}.pub")).unwrap();
+            public.insert(0, 4);
+            let key = VerifyingKey::from_sec1_bytes(&public).unwrap();
+            let signature = Signature::from_scalars(r, s).unwrap();
+            key.verify_prehash(&challenge, &signature).unwrap();
+        }
+        (Request::List, Response::List { names }) => {
+            for name in names {
+                println!("- {name}");
+            }
+        }
+        (Request::Delete { name }, Response::Delete) => {
+            std::fs::remove_file(format!("{name}.pub")).unwrap();
+        }
+        _ => panic!("The response does not match the request."),
+    }
+}
+
+#[cfg(feature = "usb")]
+fn connect() -> Box<dyn serialport::SerialPort> {
+    for info in serialport::available_ports().unwrap() {
+        let path = info.port_name;
+        if let serialport::SerialPortType::UsbPort(info) = info.port_type {
+            if info.vid == 0x16c0 && info.pid == 0x27dd {
+                let mut serial =
+                    serialport::new(path, 19200).timeout(Duration::from_secs(1)).open().unwrap();
+                serial.set_timeout(Duration::from_secs(10)).unwrap();
+                return serial;
+            }
+        }
+    }
+    panic!("no available port");
+}
+
+#[cfg(not(feature = "usb"))]
+fn connect() -> std::os::unix::net::UnixStream {
+    use std::os::unix::net::UnixStream;
+    let serial = UnixStream::connect("../../../../target/wasefire/uart0").unwrap();
+    serial.set_read_timeout(Some(Duration::from_secs(10))).unwrap();
+    serial
+}

examples/rust/exercises/interface/Cargo.toml
@@ -1,0 +1,11 @@
+[package]
+name = "interface"
+version = "0.1.0"
+authors = ["Julien Cretin <cretin@google.com>"]
+license = "Apache-2.0"
+edition = "2021"
+
+[dependencies]
+postcard = { version = "1.0.8", default-features = false, features = ["alloc"] }
+serde = { version = "1.0.189", default-features = false, features = ["alloc", "derive"] }
+wasefire = { path = "../../../../crates/prelude" }

examples/rust/exercises/part-1-sol/Cargo.toml
@@ -1,0 +1,9 @@
+[package]
+name = "part-1-sol"
+version = "0.1.0"
+authors = ["Julien Cretin <cretin@google.com>"]
+license = "Apache-2.0"
+edition = "2021"
+
+[dependencies]
+wasefire = { path = "../../../../crates/prelude" }

examples/rust/exercises/part-1/Cargo.toml
@@ -1,0 +1,9 @@
+[package]
+name = "part-1"
+version = "0.1.0"
+authors = ["Julien Cretin <cretin@google.com>"]
+license = "Apache-2.0"
+edition = "2021"
+
+[dependencies]
+wasefire = { path = "../../../../crates/prelude" }

examples/rust/exercises/part-2-sol/Cargo.toml
@@ -1,0 +1,10 @@
+[package]
+name = "part-2-sol"
+version = "0.1.0"
+authors = ["Julien Cretin <cretin@google.com>"]
+license = "Apache-2.0"
+edition = "2021"
+
+[dependencies]
+interface = { path = "../interface" }
+wasefire = { path = "../../../../crates/prelude" }

commit 7279334c207855983c4f3f473da1b7571ed18973

Use cargo metadata for applet name and target directory (#280)
This also fixes a bug introduced by #270 where the target directory is
wrong for non-example applets.

crates/xtask/Cargo.toml
@@ -8,6 +8,7 @@ edition = "2021"
 
 [dependencies]
 anyhow = "1.0.75"
+cargo_metadata = "0.18.1"
 clap = { version = "4.4.4", features = ["derive"] }
 env_logger = "0.10.0"
 lazy_static = "1.4.0"

crates/xtask/src/main.rs
@@ -23,6 +23,7 @@ use std::process::{Command, Output};
 use std::str::FromStr;
 
 use anyhow::{bail, ensure, Context, Result};
+use cargo_metadata::MetadataCommand;
 use clap::Parser;
 use lazy_static::lazy_static;
 use probe_rs::config::TargetSelector;
@@ -271,13 +272,11 @@ impl AppletOptions {
         };
         ensure!(Path::new(&dir).exists(), "{dir} does not exist");
         let wasm = {
-            // We could use `cargo metadata --no-deps --format-version=1` and parse the JSON to get
-            // both the target name and target directory.
-            let mut sed = Command::new("sed");
-            sed.args(["-n", r#"s/^name = "\(.*\)"$/\1/p"#, "Cargo.toml"]);
-            sed.current_dir(&dir);
-            let name = read_output_line(&mut sed)?.replace('-', "_");
-            format!("target/wasm32-unknown-unknown/release/{name}.wasm")
+            let metadata = MetadataCommand::new().current_dir(&dir).no_deps().exec()?;
+            let target = &metadata.target_directory;
+            assert_eq!(metadata.packages.len(), 1);
+            let name = &metadata.packages[0].name;
+            format!("{target}/wasm32-unknown-unknown/release/{name}.wasm")
         };
         let mut cargo = Command::new("cargo");
         let mut rustflags = vec![