google/wasefire

commit 7279334c207855983c4f3f473da1b7571ed18973

Use cargo metadata for applet name and target directory (#280)
This also fixes a bug introduced by #270 where the target directory is
wrong for non-example applets.

crates/xtask/src/main.rs
@@ -23,6 +23,7 @@ use std::process::{Command, Output};
 use std::str::FromStr;
 
 use anyhow::{bail, ensure, Context, Result};
+use cargo_metadata::MetadataCommand;
 use clap::Parser;
 use lazy_static::lazy_static;
 use probe_rs::config::TargetSelector;
@@ -271,13 +272,11 @@ impl AppletOptions {
         };
         ensure!(Path::new(&dir).exists(), "{dir} does not exist");
         let wasm = {
-            // We could use `cargo metadata --no-deps --format-version=1` and parse the JSON to get
-            // both the target name and target directory.
-            let mut sed = Command::new("sed");
-            sed.args(["-n", r#"s/^name = "\(.*\)"$/\1/p"#, "Cargo.toml"]);
-            sed.current_dir(&dir);
-            let name = read_output_line(&mut sed)?.replace('-', "_");
-            format!("target/wasm32-unknown-unknown/release/{name}.wasm")
+            let metadata = MetadataCommand::new().current_dir(&dir).no_deps().exec()?;
+            let target = &metadata.target_directory;
+            assert_eq!(metadata.packages.len(), 1);
+            let name = &metadata.packages[0].name;
+            format!("{target}/wasm32-unknown-unknown/release/{name}.wasm")
         };
         let mut cargo = Command::new("cargo");
         let mut rustflags = vec![

crates/xtask/Cargo.toml
@@ -8,6 +8,7 @@ edition = "2021"
 
 [dependencies]
 anyhow = "1.0.75"
+cargo_metadata = "0.18.1"
 clap = { version = "4.4.4", features = ["derive"] }
 env_logger = "0.10.0"
 lazy_static = "1.4.0"

commit accc48a25c435b3e5fc1cc18d59d97b173768ab8

Fix applet name to path conversion (#281)
Fixes a regression introduced by #280.

crates/xtask/src/main.rs
@@ -275,7 +275,7 @@ impl AppletOptions {
             let metadata = MetadataCommand::new().current_dir(&dir).no_deps().exec()?;
             let target = &metadata.target_directory;
             assert_eq!(metadata.packages.len(), 1);
-            let name = &metadata.packages[0].name;
+            let name = metadata.packages[0].name.replace('-', "_");
             format!("{target}/wasm32-unknown-unknown/release/{name}.wasm")
         };
         let mut cargo = Command::new("cargo");