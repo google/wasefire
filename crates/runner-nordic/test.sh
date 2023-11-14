b631415eb4d2ad2ab2adf0c77bf539e814eb1558
Julien Cretin <cretin@google.com>
2023-11-14 11:52:33 +0100

    Also fix the runners

--- crates/runner-nordic/test.sh
+++ crates/runner-nordic/test.sh
@@ -19,6 +19,11 @@ if [ ! -e ../../target/wasefire/applet.wasm ]; then
   mkdir -p ../../target/wasefire
   touch ../../target/wasefire/applet.wasm
 fi
+if [ ! -e ../../target/wasefire/libapplet.a ]; then
+  ( cd ../..
+    cargo xtask --native-target=thumbv7em-none-eabi applet rust hello
+  )
+fi
 cargo check --target=thumbv7em-none-eabi --features=wasm,debug
 DEFMT_LOG=trace cargo check --target=thumbv7em-none-eabi --features=wasm,debug
 cargo check --target=thumbv7em-none-eabi --features=wasm,release
