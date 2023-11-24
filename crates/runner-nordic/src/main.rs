google/wasefire

commit 9fcf9c4d9eed3e16e57cc645f86b8b7d8ff1968f

Add non-blocking mutex abstraction (#300)

crates/runner-nordic/Cargo.toml
@@ -26,6 +26,7 @@ wasefire-applet-api = { path = "../api" }
 wasefire-board-api = { path = "../board" }
 wasefire-interpreter = { path = "../interpreter", optional = true }
 wasefire-logger = { path = "../logger" }
+wasefire-mutex = { path = "../mutex" }
 wasefire-scheduler = { path = "../scheduler" }
 wasefire-store = { path = "../store" }
 

crates/runner-nordic/src/main.rs
@@ -24,12 +24,10 @@ mod storage;
 mod systick;
 mod tasks;
 
-use core::cell::RefCell;
 use core::mem::MaybeUninit;
 
 use cortex_m::peripheral::NVIC;
 use cortex_m_rt::entry;
-use critical_section::Mutex;
 #[cfg(feature = "debug")]
 use defmt_rtt as _;
 use nrf52840_hal::ccm::{Ccm, DataRate};
@@ -50,6 +48,7 @@ use usb_device::device::{StringDescriptors, UsbDevice, UsbDeviceBuilder, UsbVidP
 use usbd_serial::{SerialPort, USB_CLASS_CDC};
 use wasefire_board_api::usb::serial::Serial;
 use wasefire_board_api::{Id, Support};
+use wasefire_mutex::Mutex;
 use wasefire_scheduler::Scheduler;
 use {wasefire_board_api as board, wasefire_logger as log};
 
@@ -84,10 +83,10 @@ struct State {
 
 pub enum Board {}
 
-static STATE: Mutex<RefCell<Option<State>>> = Mutex::new(RefCell::new(None));
+static STATE: Mutex<Option<State>> = Mutex::new(None);
 
 fn with_state<R>(f: impl FnOnce(&mut State) -> R) -> R {
-    critical_section::with(|cs| f(STATE.borrow_ref_mut(cs).as_mut().unwrap()))
+    f(STATE.lock().as_mut().unwrap())
 }
 
 #[entry]
@@ -143,7 +142,7 @@ fn main() -> ! {
         State { events, buttons, gpiote, serial, timers, ccm, leds, rng, storage, uarts, usb_dev };
     // We first set the board and then enable interrupts so that interrupts may assume the board is
     // always present.
-    critical_section::with(|cs| STATE.replace(cs, Some(state)));
+    STATE.lock().replace(state);
     for &interrupt in INTERRUPTS {
         unsafe { NVIC::unmask(interrupt) };
     }

crates/mutex/Cargo.toml
@@ -1,0 +1,15 @@
+[package]
+name = "wasefire-mutex"
+version = "0.1.0-git"
+authors = ["Julien Cretin <cretin@google.com>"]
+license = "Apache-2.0"
+publish = true
+edition = "2021"
+description = "Portable non-blocking mutex"
+repository = "https://github.com/google/wasefire"
+include = ["/src"]
+keywords = ["framework", "mutex", "no-std"]
+categories = ["concurrency", "no-std"]
+
+[dependencies]
+portable-atomic = { version = "1.5.1", default-features = false }

crates/prelude/Cargo.toml
@@ -25,6 +25,7 @@ rlsf = { version = "0.2.1", default-features = false }
 sealed = { version = "0.5.0", default-features = false }
 typenum = { version = "1.17.0", default-features = false }
 wasefire-applet-api = { version = "0.6.0-git", path = "../api", features = ["wasm"] }
+wasefire-mutex = { version = "0.1.0-git", path = "../mutex" }
 zeroize = { version = "1.6.0", default-features = false, features = ["derive"], optional = true }
 
 [features]

crates/scheduler/Cargo.toml
@@ -23,6 +23,7 @@ typenum = { version = "1.17.0", default-features = false }
 wasefire-applet-api = { version = "0.6.0-git", path = "../api", features = ["host"] }
 wasefire-board-api = { version = "0.6.0-git", path = "../board" }
 wasefire-logger = { version = "0.1.4-git", path = "../logger" }
+wasefire-mutex = { version = "0.1.0-git", path = "../mutex" }
 wasefire-store = { version = "0.2.3-git", path = "../store" }
 
 [dependencies.wasefire-interpreter]

crates/mutex/src/lib.rs
@@ -1,0 +1,101 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! This crate provides non-blocking mutexes using portable-atomic.
+
+#![no_std]
+#![deny(unsafe_op_in_unsafe_fn)]
+
+use core::cell::UnsafeCell;
+use core::ops::{Deref, DerefMut};
+
+use portable_atomic::AtomicBool;
+use portable_atomic::Ordering::SeqCst;
+
+/// Non-blocking non-reentrant mutex.
+///
+/// Locking this mutex will panic if it is already locked.
+pub struct Mutex<T> {
+    lock: AtomicBool,
+    data: UnsafeCell<T>,
+}
+
+/// Locks a mutex and provides access to its content until dropped.
+pub struct MutexGuard<'a, T>(&'a Mutex<T>);
+
+unsafe impl<T: Send> Sync for Mutex<T> {}
+
+impl<T> Mutex<T> {
+    /// Creates a new mutex.
+    pub const fn new(data: T) -> Self {
+        Mutex { lock: AtomicBool::new(false), data: UnsafeCell::new(data) }
+    }
+
+    /// Tries to lock the mutex.
+    pub fn try_lock(&self) -> Option<MutexGuard<'_, T>> {
+        match self.lock.swap(true, SeqCst) {
+            false => Some(MutexGuard(self)),
+            true => None,
+        }
+    }
+
+    /// Locks the mutex.
+    ///
+    /// # Panics
+    ///
+    /// Panics if it is already locked.
+    pub fn lock(&self) -> MutexGuard<'_, T> {
+        self.try_lock().unwrap()
+    }
+}
+
+impl<'a, T> Deref for MutexGuard<'a, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.0.data.get() }
+    }
+}
+
+impl<'a, T> DerefMut for MutexGuard<'a, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { &mut *self.0.data.get() }
+    }
+}
+
+impl<'a, T> Drop for MutexGuard<'a, T> {
+    fn drop(&mut self) {
+        assert!(self.0.lock.swap(false, SeqCst));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn static_mutex() {
+        static MUTEX: Mutex<i32> = Mutex::new(42);
+        *MUTEX.lock() = 13;
+        assert_eq!(*MUTEX.lock(), 13);
+    }
+
+    #[test]
+    #[should_panic]
+    fn double_lock() {
+        let mutex = Mutex::new(42);
+        let _guard = mutex.lock();
+        mutex.lock();
+    }
+}

crates/mutex/test.sh
@@ -1,0 +1,21 @@
+#!/bin/sh
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -ex
+
+cargo check
+cargo fmt -- --check
+cargo clippy -- --deny=warnings
+cargo test

crates/prelude/src/sync.rs
@@ -13,61 +13,6 @@
 // limitations under the License.
 
 //! Provides API for mutexes and atomics.
-//!
-//! This module is currently only correct when compiled without the atomics proposal.
-#![cfg(not(target_feature = "atomics"))]
-
-use core::cell::{Cell, RefCell, RefMut};
 
 pub use portable_atomic::*;
-
-pub struct Mutex<T: ?Sized> {
-    locked: Cell<bool>,
-    data: RefCell<T>,
-}
-
-pub struct MutexGuard<'a, T: ?Sized + 'a> {
-    locked: &'a Cell<bool>,
-    data: RefMut<'a, T>,
-}
-
-// SAFETY: This is a single-threaded environment. The bound on Send comes from std::sync::Mutex, not
-// sure if relevant or needed, but it's safer to have it.
-unsafe impl<T: ?Sized + Send> Sync for Mutex<T> {}
-
-impl<T> Mutex<T> {
-    pub const fn new(value: T) -> Self {
-        Self { locked: Cell::new(false), data: RefCell::new(value) }
-    }
-
-    pub fn lock(&self) -> MutexGuard<T> {
-        self.try_lock().expect("cannot recursively acquire mutex")
-    }
-
-    pub fn try_lock(&self) -> Option<MutexGuard<T>> {
-        if self.locked.replace(true) {
-            return None;
-        }
-        Some(MutexGuard { locked: &self.locked, data: self.data.borrow_mut() })
-    }
-}
-
-impl<T: ?Sized> core::ops::Deref for MutexGuard<'_, T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        self.data.deref()
-    }
-}
-
-impl<T: ?Sized> core::ops::DerefMut for MutexGuard<'_, T> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        self.data.deref_mut()
-    }
-}
-
-impl<T: ?Sized> Drop for MutexGuard<'_, T> {
-    fn drop(&mut self) {
-        self.locked.set(false);
-    }
-}
+pub use wasefire_mutex::*;

commit bd4b68d160a4d507a8e3d818b176f5c4eef0c5ba

Disable interrupts when locking state
Fixes a regression introduced by #300. Interrupts may lock the state while it
being already locked by the main thread.

crates/runner-nordic/Cargo.toml
@@ -26,7 +26,6 @@ wasefire-applet-api = { path = "../api" }
 wasefire-board-api = { path = "../board" }
 wasefire-interpreter = { path = "../interpreter", optional = true }
 wasefire-logger = { path = "../logger" }
-wasefire-mutex = { path = "../mutex" }
 wasefire-scheduler = { path = "../scheduler" }
 wasefire-store = { path = "../store" }
 

crates/runner-nordic/src/main.rs
@@ -24,10 +24,12 @@ mod storage;
 mod systick;
 mod tasks;
 
+use core::cell::RefCell;
 use core::mem::MaybeUninit;
 
 use cortex_m::peripheral::NVIC;
 use cortex_m_rt::entry;
+use critical_section::Mutex;
 #[cfg(feature = "debug")]
 use defmt_rtt as _;
 use nrf52840_hal::ccm::{Ccm, DataRate};
@@ -48,7 +50,6 @@ use usb_device::device::{StringDescriptors, UsbDevice, UsbDeviceBuilder, UsbVidP
 use usbd_serial::{SerialPort, USB_CLASS_CDC};
 use wasefire_board_api::usb::serial::Serial;
 use wasefire_board_api::{Id, Support};
-use wasefire_mutex::Mutex;
 use wasefire_scheduler::Scheduler;
 use {wasefire_board_api as board, wasefire_logger as log};
 
@@ -83,10 +84,10 @@ struct State {
 
 pub enum Board {}
 
-static STATE: Mutex<Option<State>> = Mutex::new(None);
+static STATE: Mutex<RefCell<Option<State>>> = Mutex::new(RefCell::new(None));
 
 fn with_state<R>(f: impl FnOnce(&mut State) -> R) -> R {
-    f(STATE.lock().as_mut().unwrap())
+    critical_section::with(|cs| f(STATE.borrow_ref_mut(cs).as_mut().unwrap()))
 }
 
 #[entry]
@@ -142,7 +143,7 @@ fn main() -> ! {
         State { events, buttons, gpiote, serial, timers, ccm, leds, rng, storage, uarts, usb_dev };
     // We first set the board and then enable interrupts so that interrupts may assume the board is
     // always present.
-    STATE.lock().replace(state);
+    critical_section::with(|cs| STATE.replace(cs, Some(state)));
     for &interrupt in INTERRUPTS {
         unsafe { NVIC::unmask(interrupt) };
     }