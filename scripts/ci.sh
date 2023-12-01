92e669ae62304df6526438df6c12a01af4f228e2
Julien Cretin <cretin@google.com>
2023-12-01 11:15:04 +0100

    Add CI to opensk branch

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -19,45 +19,18 @@ set -e
 
 # This script runs the continuous integration tests.
 
-for lang in $(ls examples); do
-  for name in $(ls examples/$lang); do
-    [ $lang = assemblyscript -a $name = node_modules ] && continue
-    [ $lang = assemblyscript -a $name = api.ts ] && continue
-    [ $lang = rust -a $name = exercises ] && continue
-    x cargo xtask applet $lang $name
-    x cargo xtask --release applet $lang $name
-    [ $lang = rust ] || continue
-    i "Run lints and tests for applet $name"
-    ( cd examples/rust/$name
-      x cargo fmt -- --check
-      x cargo clippy --lib --target=wasm32-unknown-unknown -- --deny=warnings
-      if package_features | grep -q '^test$'; then
-        x cargo clippy --features=test -- --deny=warnings
-        grep -q '^mod tests {$' src/lib.rs && x cargo test --features=test
-        [ -e src/main.rs ] && x env WASEFIRE_DEBUG=1 cargo run --features=test
-      fi
-    )
-  done
-done
-
-for crate in $(ls crates); do
-  name=${crate#runner-}
-  [ $crate = $name ] && continue
-  x cargo xtask runner $name --log=trace
-  x cargo xtask --release runner $name
-done
-
-for dir in $(git ls-files '*/test.sh'); do
-  dir=$(dirname $dir)
-  i "Run tests in $dir"
-  ( cd $dir && ./test.sh )
-done
-
-./scripts/hwci.sh host --no-default-features
-
-i "Build the book"
-WASEFIRE_WRAPPER_EXEC=n ./scripts/wrapper.sh mdbook
-( cd book && ../scripts/wrapper.sh mdbook build 2>/dev/null )
+x cargo xtask applet rust opensk
+x cargo xtask --release applet rust opensk
+( cd examples/rust/opensk
+  x cargo fmt -- --check
+  # TODO: Enable this back at some point.
+  # x cargo clippy --lib --target=wasm32-unknown-unknown -- --deny=warnings
+  if package_features | grep -q '^test$'; then
+    # TODO: Enable this back at some point.
+    # x cargo clippy --features=test -- --deny=warnings
+    grep -q '^mod tests {$' src/lib.rs && x cargo test --features=test
+  fi
+)
 
 git diff --exit-code \
   || e "Tracked files were modified and/or untracked files were created"
@@ -67,6 +40,3 @@ x ./scripts/wrapper.sh taplo lint \
 --schema-catalog=file://"$PWD"/target/schemastore/catalog.json
 x ./scripts/wrapper.sh taplo format
 git diff --exit-code || e "TOML files are not well formatted"
-
-x ./scripts/sync.sh
-git diff --exit-code || e "Generated content is not in sync"
