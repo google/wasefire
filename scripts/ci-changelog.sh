469d22fc8daa73869ffd338c03156342bf74cbe1
Julien Cretin <cretin@google.com>
2024-05-24 16:36:12 +0200

    Permit api-{desc,macro} to have dependencies with default features

--- scripts/ci-changelog.sh
+++ scripts/ci-changelog.sh
@@ -26,25 +26,28 @@ for dir in $(find crates -name Cargo.toml -printf '%h\n' | sort); do
     [ -e test.sh ] || e "test.sh for $dir is missing"
     $publish || exit 0
     [ -e CHANGELOG.md ] || e "CHANGELOG.md for $dir is missing"
-    [ "$(package_include)" = '["/src"]' ] \
-      || e "Cargo.toml should only include the src directory"
+    [ "$(package_include)" = '["/src"]' ] || e "Cargo.toml should only include the src directory"
     [ -z "$(package_exclude)" ] || e "Cargo.toml should not exclude anything"
     ref=$(git log -n1 --pretty=format:%H origin/main.. -- CHANGELOG.md)
     [ -n "$ref" ] || ref=origin/main
-    git diff --quiet $ref -- Cargo.toml src \
-      || e "CHANGELOG.md for $dir is not up-to-date"
+    git diff --quiet $ref -- Cargo.toml src || e "CHANGELOG.md for $dir is not up-to-date"
     ver="$(sed -n '3s/^## //p' CHANGELOG.md)"
     [ -n "$ver" ] || e "CHANGELOG.md for $dir does not start with version"
     [ "$(package_version)" = "$ver" ] \
       || e "CHANGELOG.md and Cargo.toml for $dir have different versions"
-    if [ $dir != crates/prelude ]; then
-      package_features | grep -q '^default$' \
-        && e "Cargo.toml for $dir has default features"
-    fi
-    case $dir in crates/cli|crates/cli-tools) exit 0 ;; esac
-    sed -n '/^\[dependencies]$/,/^$/{/^wasefire-/d;/^[a-z]/!d;'\
+    crate=${dir#crates/}
+    case $crate in
+      # Those crates are allowed to enable features by default.
+      prelude) ;;
+      *) package_features | grep -q '^default$' && e "Cargo.toml for $dir has default features" ;;
+    esac
+    case $dir in
+      # Those crates are allowed to have dependencies with default features.
+      api-desc|api-macro|cli|cli-tools) ;;
+      *) sed -n '/^\[dependencies]$/,/^$/{/^wasefire-/d;/^[a-z]/!d;'\
 '/default-features = false/d;p;q1};/^\[dependencies\.wasefire-/d;'\
 '/^\[dependencies\./{h;:a;n;/default-features = false/d;/^$/{g;p;q1};ba}' \
-Cargo.toml || e "Cargo.toml for $dir doesn't disable default-features"
+Cargo.toml || e "Cargo.toml for $dir doesn't disable default-features" ;;
+    esac
   )
 done
