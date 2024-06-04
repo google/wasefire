google/wasefire

commit 32dbefd19565f765c26d49383284617790e3f565

Redesign the platform protocol (#506)
Return `ApiResult<T>` instead of `Api<Response>`.

crates/protocol/crates/schema/src/main.rs
@@ -20,18 +20,27 @@ use std::process::Command;
 
 use anyhow::{bail, ensure, Context, Result};
 use wasefire_cli_tools::{cmd, fs};
-use wasefire_protocol::{Api, Request, Response};
+use wasefire_error::Error;
+use wasefire_protocol::{Api, Descriptor, Request, Response, DESCRIPTORS, VERSION};
 use wasefire_wire::schema::{View, ViewEnum, ViewFields};
-use wasefire_wire::Wire;
-use yoke::{Yoke, Yokeable};
+use wasefire_wire::{Wire, Yoke};
 
 fn main() -> Result<()> {
+    let base = std::env::var("BASE_REF").unwrap_or_else(|_| {
+        format!("origin/{}", std::env::var("GITHUB_BASE_REF").as_deref().unwrap_or("main"))
+    });
     let new = Schema::new();
     new.write().context("writing bin")?;
     new.print().context("printing txt")?;
-    let old = Schema::old()?;
+    let hash = cmd::output_line(Command::new("git").args(["rev-parse", &base]))?;
+    if hash == "13a0d6eb5ed261c2c0e89744bb339b99e24d2e2a" {
+        return Ok(());
+    }
+    let old = Schema::old(&base)?;
+    check(&old.get().result, &new.result).context("checking result")?;
     check(&old.get().request, &new.request).context("checking request")?;
     check(&old.get().response, &new.response).context("checking response")?;
+    check_versions(old.get(), &new).context("checking versions")?;
     Ok(())
 }
 
@@ -49,29 +58,74 @@ fn check(old: &View, new: &View) -> Result<()> {
             ensure!(old_api == new_api, "incompatible API for {tag}: {old_api} != {new_api}");
         }
     }
-    #[cfg(feature = "full")]
+    #[cfg(feature = "host")]
     ensure!(old_map.is_empty(), "deleted tag {}", old_map.keys().next().unwrap());
     Ok(())
 }
 
-#[derive(Debug, Wire, Yokeable)]
-struct Schema<'a> {
-    request: View<'a>,
-    response: View<'a>,
+fn check_versions(old: &Schema, new: &Schema) -> Result<()> {
+    let mut old_map = HashMap::new();
+    for Descriptor { tag, versions } in &old.descriptors {
+        ensure!(old_map.insert(tag, *versions).is_none(), "duplicate tag {tag}");
+    }
+    let mut changed = false;
+    let old_version = old.version;
+    let new_version = new.version;
+    for Descriptor { tag, versions: new } in &new.descriptors {
+        #[cfg(feature = "device")]
+        ensure!(new.max.is_none(), "max version is defined for {tag}");
+        match old_map.remove(&tag) {
+            Some(old) => {
+                ensure!(new.min == old.min, "min version changed for {tag}");
+                #[cfg(feature = "host")]
+                match (old.max, new.max) {
+                    (None, Some(new)) if new == old_version => (),
+                    (None, Some(_)) => bail!("max version for {tag} must be {old_version}"),
+                    (old, new) if old == new => (),
+                    _ => bail!("max version changed for {tag}"),
+                }
+            }
+            None => {
+                changed = true;
+                ensure!(new.min == new_version, "min version for {tag} must be {new_version}");
+            }
+        }
+    }
+    changed |= !old_map.is_empty();
+    #[cfg(feature = "host")]
+    ensure!(old_map.is_empty(), "deleted tag {}", old_map.keys().next().unwrap());
+    #[cfg(feature = "host")]
+    let _ = changed;
+    #[cfg(feature = "device")]
+    ensure!(new_version == old_version + changed as u32, "invalid version (must bump: {changed})");
+    Ok(())
 }
 
-type OwnedSchema = Yoke<Schema<'static>, Box<[u8]>>;
+#[derive(Debug, Wire)]
+struct Schema<'a> {
+    result: View<'a>,   // Result<(), Error>
+    request: View<'a>,  // Api<Request>
+    response: View<'a>, // Api<Response>
+    version: u32,
+    descriptors: Vec<Descriptor>,
+}
 
 impl Schema<'static> {
     fn new() -> Self {
-        Schema { request: View::new::<Api<Request>>(), response: View::new::<Api<Response>>() }
+        Schema {
+            result: View::new::<Result<(), Error>>(),
+            request: View::new::<Api<Request>>(),
+            response: View::new::<Api<Response>>(),
+            version: VERSION,
+            descriptors: DESCRIPTORS.to_vec(),
+        }
     }
 
-    fn old() -> Result<OwnedSchema> {
+    fn old(base: &str) -> Result<Yoke<Schema<'static>>> {
         let mut git = Command::new("git");
-        git.args(["show", &format!("origin/main:./{SIDE}.bin")]);
+        git.args(["show", &format!("{base}:./{SIDE}.bin")]);
         let data = cmd::output(&mut git)?.stdout.into_boxed_slice();
-        Yoke::try_attach_to_cart(data, |data| Ok(wasefire_wire::decode(data)?))
+        Ok(wasefire_wire::decode_yoke(data)?)
     }
 
     fn write(&self) -> Result<()> {
@@ -80,25 +134,32 @@ impl Schema<'static> {
 
     fn print(&self) -> Result<()> {
         let mut output = String::new();
-        let request = get_enum(&self.request).context("in request")?;
-        let response = get_enum(&self.response).context("in response")?;
-        ensure!(request.len() == response.len(), "API length mismatch");
-        for (request, response) in request.iter().zip(response.iter()) {
-            ensure!(request.0 == response.0, "API name mismatch");
-            ensure!(request.1 == response.1, "API tag mismatch");
+        writeln!(&mut output, "result: {}", self.result)?;
+        writeln!(&mut output, "version: {}", self.version)?;
+        let request = get_enum(&self.request).context("in request")?.iter();
+        let mut response = get_enum(&self.response).context("in response")?.iter();
+        let mut descriptor = self.descriptors.iter();
+        for request in request {
             let name = request.0;
             let tag = request.1;
+            let response = response.next().unwrap();
+            let descriptor = descriptor.next().unwrap();
+            assert_eq!(response.0, name);
+            assert_eq!(response.1, tag);
+            assert_eq!(descriptor.tag, tag);
             let request = ViewFields(&request.2);
             let response = ViewFields(&response.2);
-            writeln!(&mut output, "{name}={tag}: {request} -> {response}")?;
+            writeln!(&mut output, "{descriptor} {name}: {request} -> {response}")?;
         }
+        assert!(response.next().is_none());
+        assert!(descriptor.next().is_none());
         fs::write(format!("{SIDE}.txt"), &output)
     }
 }
 
-#[cfg(feature = "full")]
+#[cfg(feature = "host")]
 const SIDE: &str = "host";
-#[cfg(not(feature = "full"))]
+#[cfg(feature = "device")]
 const SIDE: &str = "device";
 
 fn get_enum<'a>(x: &'a View<'a>) -> Result<&'a ViewEnum<'a>> {
@@ -139,10 +200,10 @@ mod tests {
             Foo,
         }
         // Removing a variant is accepted on the device.
-        #[cfg(not(feature = "full"))]
+        #[cfg(feature = "device")]
         assert!(check(&View::new::<Old>(), &View::new::<New>()).is_ok());
         // But rejected on the host.
-        #[cfg(feature = "full")]
+        #[cfg(feature = "host")]
         assert!(check(&View::new::<Old>(), &View::new::<New>()).is_err());
     }
 

examples/rust/protocol/host/src/main.rs
@@ -19,8 +19,9 @@ use anyhow::{bail, Context, Result};
 use clap::Parser;
 use rusb::GlobalContext;
 use wasefire_protocol::applet::{self, AppletId};
-use wasefire_protocol::{Api, Request, Response};
+use wasefire_protocol::{self as service, Api, ApiResult, Request, Service};
 use wasefire_protocol_usb::{self as rpc, Connection};
+use wasefire_wire::Yoke;
 
 mod tests;
 
@@ -49,17 +50,11 @@ fn main() -> Result<()> {
             let mut request = Vec::new();
             std::io::stdin().read_to_end(&mut request)?;
             let request = applet::Request { applet_id: AppletId, request: &request };
-            send(&connection, &Api::<Request>::AppletRequest(request))?;
-            receive(&connection, |response| Ok(matches!(response, Api::AppletRequest(()))))?;
-            send(&connection, &Api::<Request>::AppletResponse(AppletId))?;
-            receive(&connection, |response| match response {
-                Api::AppletResponse(applet::Response { response }) => {
-                    let response = response.context("no applet response")?;
-                    print!("{}", std::str::from_utf8(response).unwrap());
-                    Ok(true)
-                }
-                _ => Ok(false),
-            })
+            call::<service::AppletRequest>(&connection, request)?.get();
+            let response = call::<service::AppletResponse>(&connection, AppletId)?;
+            let response = response.get().response.context("no applet response")?;
+            print!("{}", std::str::from_utf8(response).unwrap());
+            Ok(())
         }
         Flags::Tunnel { delimiter } => {
             let delimiter = delimiter.as_bytes();
@@ -90,20 +85,24 @@ fn send(connection: &Connection<GlobalContext>, request: &Api<Request>) -> Resul
     Ok(())
 }
 
-fn receive(
-    connection: &Connection<GlobalContext>, process: impl FnOnce(&Api<Response>) -> Result<bool>,
-) -> Result<()> {
-    let response = connection.receive(TIMEOUT).context("receiving response")?;
-    let response = Api::<Response>::decode(&response).context("decoding response")?;
-    if !process(&response)? {
-        match response {
-            Api::DeviceError(error) => bail!("error response: {error}"),
-            _ => bail!("invalid response: {response:?}"),
-        }
-    }
-    Ok(())
+fn receive<T: Service>(
+    connection: &Connection<GlobalContext>,
+) -> Result<Yoke<T::Response<'static>>> {
+    let response = connection.receive(TIMEOUT).context("receiving response")?.into_boxed_slice();
+    ApiResult::<T>::decode_yoke(response).context("decoding response")?.try_map(|x| match x {
+        ApiResult::Ok(x) => Ok(x),
+        ApiResult::Err(error) => bail!("error response: {error}"),
+    })
+}
+
+fn call<T: Service>(
+    connection: &Connection<GlobalContext>, request: T::Request<'_>,
+) -> Result<Yoke<T::Response<'static>>> {
+    send(connection, &T::request(request))?;
+    receive::<T>(connection)
 }
 
 fn read_tunnel(connection: &Connection<GlobalContext>) -> Result<()> {
-    receive(connection, |x| Ok(matches!(x, Api::AppletTunnel(()))))
+    let () = receive::<service::AppletTunnel>(connection)?.get();
+    Ok(())
 }

crates/protocol/Cargo.toml
@@ -12,16 +12,17 @@ keywords = ["embedded", "framework", "no-std"]
 categories = ["embedded", "no-std"]
 
 [package.metadata.docs.rs]
-all-features = true
+features = ["device", "host"]
 
 [dependencies]
 wasefire-error = { version = "0.1.1-git", path = "../error" }
 wasefire-wire = { version = "0.1.0-git", path = "../wire" }
 
 [features]
+_descriptor = []
 _exhaustive = []
-full = []
-std = ["wasefire-wire/std"]
+device = []
+host = []
 
 [lints]
 clippy.unit-arg = "allow"

crates/protocol/crates/schema/Cargo.toml
@@ -9,12 +9,13 @@ edition = "2021"
 [dependencies]
 anyhow = "1.0.86"
 wasefire-cli-tools = { path = "../../../cli-tools" }
-wasefire-protocol = { path = "../..", features = ["std"] }
-wasefire-wire = { path = "../../../wire", features = ["schema", "std"] }
-yoke = { version = "0.7.3", features = ["derive"] }
+wasefire-error = { path = "../../../error" }
+wasefire-protocol = { path = "../..", features = ["_descriptor"] }
+wasefire-wire = { path = "../../../wire", features = ["schema"] }
 
 [features]
-full = ["wasefire-protocol/full"]
+device = ["wasefire-protocol/device"]
+host = ["wasefire-protocol/host"]
 
 [lints]
 clippy.unit-arg = "allow"

crates/protocol/crates/schema/test.sh
@@ -19,5 +19,5 @@ set -e
 
 test_helper
 
-cargo test --bin=schema --features=full
-cargo test --bin=schema
+cargo test --bin=schema --features=host
+cargo test --bin=schema --features=device

crates/protocol/src/lib.rs
@@ -16,7 +16,9 @@
 //!
 //! This crate defines a high-level protocol between a host and a device. The host initiates
 //! requests and the device responds. Requests and responses use the same [`Api`] but with a
-//! different type parameter: `Api<Request>` and `Api<Response>` respectively.
+//! different type parameter: `Api<Request>` and `Api<Response>` respectively. However, while the
+//! host sends an `Api<Request>`, the device responds with an `ApiResult<T>` where `T` is the
+//! service of the request.
 //!
 //! This high-level protocol is eventually wrapped in a lower-level protocol for a given transport,
 //! for example USB. The host should provide enough time for the device to respond, but should
@@ -26,6 +28,7 @@
 
 #![no_std]
 #![feature(doc_auto_cfg)]
+#![feature(macro_metavar_expr)]
 #![feature(never_type)]
 
 extern crate alloc;
@@ -34,65 +37,165 @@ use alloc::boxed::Box;
 
 use wasefire_error::Error;
 use wasefire_wire::Wire;
+#[cfg(feature = "host")]
+use wasefire_wire::Yoke;
 
 pub mod applet;
-mod logic;
 
-/// Message direction between host and device.
-pub trait Direction: logic::Direction {}
+/// Service description.
+pub trait Service: 'static {
+    /// Range of versions implementing this service.
+    #[cfg(feature = "host")]
+    const VERSIONS: Versions;
+    type Request<'a>: Wire<'a>;
+    type Response<'a>: Wire<'a>;
+    #[cfg(feature = "host")]
+    fn request(x: Self::Request<'_>) -> Api<Request>;
+}
 
-/// Requests from host to device.
 #[derive(Debug)]
 pub enum Request {}
-impl Direction for Request {}
-impl logic::Direction for Request {
-    type Type<'a, T: logic::Service> = T::Request<'a>;
+impl sealed::Direction for Request {
+    type Type<'a, T: Service> = T::Request<'a>;
 }
 
-/// Responses from device to host.
 #[derive(Debug)]
+#[cfg(feature = "_descriptor")]
 pub enum Response {}
-impl Direction for Response {}
-impl logic::Direction for Response {
-    type Type<'a, T: logic::Service> = T::Response<'a>;
+#[cfg(feature = "_descriptor")]
+impl sealed::Direction for Response {
+    type Type<'a, T: Service> = T::Response<'a>;
 }
 
-impl<'a, T: Direction> Api<'a, T> {
+mod sealed {
+    pub trait Direction: 'static {
+        type Type<'a, T: crate::Service>: wasefire_wire::Wire<'a>;
+    }
+}
+
+#[derive(Wire)]
+#[wire(static = T, range = 2)]
+pub enum ApiResult<'a, T: Service> {
+    #[wire(tag = 0)]
+    Ok(T::Response<'a>),
+    #[wire(tag = 1)]
+    Err(Error),
+}
+
+impl<'a> Api<'a, Request> {
+    #[cfg(feature = "host")]
     pub fn encode(&self) -> Result<Box<[u8]>, Error> {
         wasefire_wire::encode(self)
     }
 
+    #[cfg(feature = "device")]
     pub fn decode(data: &'a [u8]) -> Result<Self, Error> {
         wasefire_wire::decode(data)
     }
 }
 
+impl<'a, T: Service> ApiResult<'a, T> {
+    #[cfg(feature = "device")]
+    pub fn encode(&self) -> Result<Box<[u8]>, Error> {
+        wasefire_wire::encode(self)
+    }
+
+    #[cfg(feature = "host")]
+    pub fn decode(data: &[u8]) -> Result<ApiResult<T>, Error> {
+        wasefire_wire::decode(data)
+    }
+
+    #[cfg(feature = "host")]
+    pub fn decode_yoke(data: Box<[u8]>) -> Result<Yoke<ApiResult<'static, T>>, Error> {
+        wasefire_wire::decode_yoke(data)
+    }
+}
+
+#[derive(Debug, Copy, Clone, Wire)]
+#[cfg(any(feature = "host", feature = "_descriptor"))]
+pub struct Versions {
+    pub min: u32,
+    pub max: Option<u32>,
+}
+
+#[cfg(feature = "host")]
+impl Versions {
+    pub fn contains(&self, version: u32) -> Result<bool, Error> {
+        match (self.min, self.max) {
+            // Deprecated service. The device needs to be within the range.
+            (min, Some(max)) => Ok((min ..= max).contains(&version)),
+            // The device is too old.
+            (min, None) if version < min => Ok(false),
+            // The device is newer than the host, so we don't know if the service has been
+            // deprecated for that device.
+            (_, None) if VERSION < version => Err(Error::world(wasefire_error::Code::OutOfBounds)),
+            // The device is older than the host but recent enough for the service.
+            _ => Ok(true),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Wire)]
+#[cfg(feature = "_descriptor")]
+pub struct Descriptor {
+    pub tag: u32,
+    pub versions: Versions,
+}
+
+#[cfg(feature = "_descriptor")]
+impl core::fmt::Display for Descriptor {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        let Descriptor { tag, versions: Versions { min, max } } = *self;
+        match max {
+            Some(max) => write!(f, "{tag} [{min} - {max}]"),
+            None => write!(f, "{tag} [{min} -]"),
+        }
+    }
+}
+
 macro_rules! api {
-    ($(#![$api:meta])* next = $next:literal; $(
-        $(#[doc = $doc:literal])* $(#[cfg($cfg:meta)])*
-        $tag:literal $Name:ident: $request:ty => $response:ty
+    ($(#![$api:meta])* version = $version:literal; next = $next:literal; $(
+        $(#[doc = $doc:literal])*
+        $tag:literal [$min:literal - $($max:literal)?] $Name:ident: $request:ty => $response:ty
     ),*$(,)?) => {
-        $(#[$api])*
-        #[derive(Debug, Wire)]
+        $(#[$api])* #[derive(Debug, Wire)]
         #[wire(static = T)]
-        #[cfg_attr(feature = "full", wire(range = $next))]
+        #[cfg_attr(feature = "host", wire(range = $next))]
         #[cfg_attr(not(feature = "_exhaustive"), non_exhaustive)]
-        pub enum Api<'a, T: Direction> {
+        pub enum Api<'a, T: sealed::Direction> {
             $(
-                $(#[doc = $doc])* $(#[cfg($cfg)])* #[wire(tag = $tag)]
+                $(#[doc = $doc])* $(#[cfg(feature = "host")] ${ignore($max)})? #[wire(tag = $tag)]
                 $Name(T::Type<'a, $Name>),
             )*
         }
         $(
-            $(#[cfg($cfg)])* #[derive(Debug)]
+            $(#[doc = $doc])* $(#[cfg(feature = "host")] ${ignore($max)})? #[derive(Debug)]
             pub enum $Name {}
-            $(#[cfg($cfg)])*
-            impl logic::Service for $Name {
+            $(#[cfg(feature = "host")] ${ignore($max)})?
+            impl Service for $Name {
+                #[cfg(feature = "host")]
+                const VERSIONS: Versions = api!(versions $min $($max)?);
                 type Request<'a> = $request;
                 type Response<'a> = $response;
+                #[cfg(feature = "host")]
+                fn request(x: Self::Request<'_>) -> Api<Request> { Api::$Name(x) }
             }
         )*
+        /// Device API version (or maximum supported device API version for host).
+        pub const VERSION: u32 = $version;
+        #[cfg(feature = "_descriptor")]
+        pub const DESCRIPTORS: &'static [Descriptor] = &[
+            $(
+                $(#[cfg(feature = "host")] ${ignore($max)})?
+                Descriptor { tag: $tag, versions: api!(versions $min $($max)?) },
+            )*
+        ];
     };
+
+    (max) => (None);
+    (max $max:literal) => (Some($max));
+
+    (versions $min:literal $($max:literal)?) => (Versions { min: $min, max: api!(max $($max)?) });
 }
 
 api! {
@@ -100,22 +203,21 @@ api! {
     //!
     //! Variants gated by the `full` feature are deprecated. They won't be used by new devices.
     //! However, to support older devices, the host must be able to use them.
+    version = 0;
     next = 5;
 
-    /// Errors reported by the device.
-    ///
-    /// This may be returned regardless of the request type.
-    0 DeviceError: ! => Error,
+    /// Returns the device API version.
+    0 [0 -] ApiVersion: () => u32,
 
     /// Sends a request to an applet.
-    1 AppletRequest: applet::Request<'a> => (),
+    1 [0 -] AppletRequest: applet::Request<'a> => (),
 
     /// Reads a response from an applet.
-    2 AppletResponse: applet::AppletId => applet::Response<'a>,
+    2 [0 -] AppletResponse: applet::AppletId => applet::Response<'a>,
 
     /// Reboots the platform.
-    3 PlatformReboot: () => !,
+    3 [0 -] PlatformReboot: () => !,
 
     /// Starts a direct tunnel with an applet.
-    4 AppletTunnel: applet::Tunnel<'a> => (),
+    4 [0 -] AppletTunnel: applet::Tunnel<'a> => (),
 }

commit 91b40a02cb042ab00eef4b0ae58bcfe380c8714a

Fix CI on schedule broken by #506

crates/protocol/crates/schema/src/main.rs
@@ -134,7 +134,7 @@ impl Schema<'static> {
         use std::env::VarError::*;
         Ok(match var("GITHUB_EVENT_NAME").as_deref() {
             Ok("pull_request") => format!("origin/{}", var("GITHUB_BASE_REF")?),
-            Ok("push") => format!("origin/{}", var("GITHUB_REF_NAME")?),
+            Ok("push" | "schedule") => format!("origin/{}", var("GITHUB_REF_NAME")?),
             Ok(x) => bail!("unexpected GITHUB_EVENT_NAME {x:?}"),
             Err(NotPresent) => match var("BASE_REF") {
                 Ok(x) => x,