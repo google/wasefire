c850dbf7ed6964a076cfd9f8e13097c309accadd
Raushan Raj <shemant.raj38@gmail.com>
2024-07-18 00:27:48 +0530

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,6 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+echo "hello ############"
+printenv
 set -e
 
 # This script installs the dependencies to run xtask. Any other dependencies
