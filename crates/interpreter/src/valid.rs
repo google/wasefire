618cdab94f3b99b4699869fe33c12ad33047d39d
Zhou Fang <33002388+zhou-w-fang@users.noreply.github.com>
2024-07-22 23:52:18 -0700

    fix: use hashbrown::HashMap

--- crates/interpreter/src/valid.rs
+++ crates/interpreter/src/valid.rs
@@ -12,10 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use alloc::collections::{BTreeSet, HashMap};
+use alloc::collections::BTreeSet;
 use alloc::vec;
 use alloc::vec::Vec;
 use core::cmp::Ordering;
+
+use hashbrown::HashMap;
 
 use crate::error::*;
 use crate::syntax::*;
