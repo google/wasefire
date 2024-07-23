0b7cb4f75652cd8806e9687725a79a0b2ebbbed2
Zhou Fang <33002388+zhou-w-fang@users.noreply.github.com>
2024-07-22 23:43:49 -0700

    fix: minor

--- crates/interpreter/src/valid.rs
+++ crates/interpreter/src/valid.rs
@@ -12,11 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use alloc::collections::BTreeSet;
+use alloc::collections::{BTreeSet, HashMap};
 use alloc::vec;
 use alloc::vec::Vec;
 use core::cmp::Ordering;
-use std::collections::HashMap;
 
 use crate::error::*;
 use crate::syntax::*;
