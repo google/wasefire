dfa1258d8c0712611f95af1bc81742bc14f5edbf
Zhou Fang <33002388+zhou-w-fang@users.noreply.github.com>
2024-10-16 21:35:08 -0700

    mask

--- crates/interpreter/src/side_table.rs
+++ crates/interpreter/src/side_table.rs
@@ -33,10 +33,10 @@ pub struct SideTableEntryView {
 
 #[allow(dead_code)] // TODO(dev/fast-interp)
 impl SideTableEntry {
-    const DELTA_IP_MASK: u32 = 0x7ff;
-    const DELTA_STP_MASK: u32 = 0x7ffff800; // 0x7fff << 11
-    const VAL_CNT_MASK: u32 = 0x78000000; // 0x7 << 26
-    const POP_CNT_MASK: u32 = 0x60000000; // 0x7 << 29
+    const DELTA_IP_MASK: u32 = 0x7FFF;
+    const DELTA_STP_MASK: u32 = 0x7FFF << 15;
+    const VAL_CNT_MASK: u32 = 0x1 << 30;
+    const POP_CNT_MASK: u32 = 0x1 << 31;
 
     pub fn new(view: SideTableEntryView) -> Result<Self, Error> {
         let mut fields = 0;
