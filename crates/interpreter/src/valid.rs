2efa29b5cb943bd04caa37236d4627a5b05d31fe
Zhou Fang <33002388+zhou-w-fang@users.noreply.github.com>
2024-11-16 19:04:43 -0800

    fix: subtraction overflow

--- crates/interpreter/src/valid.rs
+++ crates/interpreter/src/valid.rs
@@ -466,7 +466,7 @@ impl SideTable {
             eprintln!("side-table negative stack delta {source} - {target_without_result}");
             return Err(unsupported(if_debug!(Unsupported::SideTable)));
         };
-        u32::try_from(delta - target.result).map_err(|_| {
+        u32::try_from((delta - target.result).max(0)).map_err(|_| {
             #[cfg(feature = "debug")]
             eprintln!("side-table pop_cnt overflow {delta}");
             unsupported(if_debug!(Unsupported::SideTable))
