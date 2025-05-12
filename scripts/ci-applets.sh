d6fa1583f6fa17c4777341ced5908a90b4a9f58e
Julien Cretin <cretin@google.com>
2025-05-12 15:17:57 +0200

    review

--- scripts/ci-applets.sh
+++ scripts/ci-applets.sh
@@ -25,7 +25,11 @@ for lang in $(ls examples); do
     [ $lang = assemblyscript -a $name = node_modules ] && continue
     [ $lang = assemblyscript -a $name = api.ts ] && continue
     [ $lang = rust -a $name = exercises ] && continue
-    [ $lang = rust -a $name = opensk ] && continue
+    if [ $lang = rust -a $name = opensk ]; then
+      x cargo xtask --native-target=thumbv7em-none-eabi applet $lang $name
+      x cargo xtask --native-target=thumbv7em-none-eabi --release applet $lang $name
+      continue
+    fi
     x cargo xtask applet $lang $name
     x cargo xtask --release applet $lang $name
   done
