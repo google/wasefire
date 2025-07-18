baf08501d734310ead9a5cea27db65a52ac0118c
Julien Cretin <cretin@google.com>
2025-07-18 10:18:23 +0200

    Use ECDSA and ECDH high-level APIs in OpenSK example

--- examples/rust/opensk/src/env/crypto/ecdsa.rs
+++ examples/rust/opensk/src/env/crypto/ecdsa.rs
@@ -17,7 +17,7 @@ use alloc::vec::Vec;
 use opensk_lib::api::crypto::ecdsa::{self, Ecdsa};
 use opensk_lib::api::crypto::{EC_FIELD_SIZE, EC_SIGNATURE_SIZE, HASH_SIZE};
 use opensk_lib::api::rng::Rng;
-use wasefire::crypto::ec::{EcdsaPrivate, EcdsaPublic, EcdsaSignature, Int, P256};
+use wasefire::crypto::ecdsa::{P256, Private, Public};
 
 use crate::env::WasefireEnv;
 
@@ -27,9 +27,12 @@ impl Ecdsa for WasefireEnv {
     type Signature = Signature;
 }
 
-pub(crate) struct SecretKey(EcdsaPrivate<P256>);
-pub(crate) struct PublicKey(EcdsaPublic<P256>);
-pub(crate) struct Signature(EcdsaSignature<P256>);
+pub(crate) struct SecretKey(Private<P256>);
+pub(crate) struct PublicKey(Public<P256>);
+pub(crate) struct Signature {
+    r: [u8; 32],
+    s: [u8; 32],
+}
 
 impl ecdsa::SecretKey for SecretKey {
     type PublicKey = PublicKey;
@@ -37,23 +40,25 @@ impl ecdsa::SecretKey for SecretKey {
 
     fn random(_rng: &mut impl Rng) -> Self {
         // TODO: Use the rng argument.
-        SecretKey(EcdsaPrivate::random().unwrap())
-    }
-
-    fn from_slice(bytes: &[u8; EC_FIELD_SIZE]) -> Option<Self> {
-        Some(SecretKey(EcdsaPrivate::from_non_zero_scalar((*bytes).into()).ok()?))
+        SecretKey(Private::generate().unwrap())
     }
 
     fn public_key(&self) -> Self::PublicKey {
-        PublicKey(self.0.public_key())
+        PublicKey(self.0.public().unwrap())
     }
 
     fn sign(&self, message: &[u8]) -> Self::Signature {
-        Signature(self.0.sign(message).unwrap())
+        let mut signature = Signature { r: [0; 32], s: [0; 32] };
+        self.0.sign(message, &mut signature.r, &mut signature.s).unwrap();
+        signature
     }
 
-    fn to_slice(&self, bytes: &mut [u8; EC_FIELD_SIZE]) {
-        *bytes = (*self.0.private_key()).into();
+    fn export(&self) -> Vec<u8> {
+        self.0.export().unwrap().into_vec()
+    }
+
+    fn import(bytes: &[u8]) -> Option<Self> {
+        Some(SecretKey(Private::import(bytes).ok()?))
     }
 }
 
@@ -61,38 +66,38 @@ impl ecdsa::PublicKey for PublicKey {
     type Signature = Signature;
 
     fn from_coordinates(x: &[u8; EC_FIELD_SIZE], y: &[u8; EC_FIELD_SIZE]) -> Option<Self> {
-        Some(PublicKey(EcdsaPublic::from_coordinates((*x).into(), (*y).into()).ok()?))
+        Some(PublicKey(Public::import(x, y).ok()?))
     }
 
     fn verify(&self, message: &[u8], signature: &Self::Signature) -> bool {
-        self.0.verify(message, &signature.0).unwrap()
+        self.0.verify(message, &signature.r, &signature.s).unwrap()
     }
 
     fn verify_prehash(&self, prehash: &[u8; HASH_SIZE], signature: &Self::Signature) -> bool {
-        self.0.verify_prehash(prehash.into(), &signature.0).unwrap()
+        self.0.verify_prehash(prehash, &signature.r, &signature.s).unwrap()
     }
 
     fn to_coordinates(&self, x: &mut [u8; EC_FIELD_SIZE], y: &mut [u8; EC_FIELD_SIZE]) {
-        *x = (*self.0.x()).into();
-        *y = (*self.0.y()).into();
+        self.0.export(x, y).unwrap();
     }
 }
 
 impl ecdsa::Signature for Signature {
     fn from_slice(bytes: &[u8; EC_SIGNATURE_SIZE]) -> Option<Self> {
-        let r = Int::<P256>::clone_from_slice(&bytes[.. 32]);
-        let s = Int::<P256>::clone_from_slice(&bytes[32 ..]);
-        Some(Signature(EcdsaSignature::from_components(r, s)))
+        let mut signature = Signature { r: [0; 32], s: [0; 32] };
+        signature.r.copy_from_slice(&bytes[.. 32]);
+        signature.s.copy_from_slice(&bytes[32 ..]);
+        Some(signature)
     }
 
     fn to_slice(&self, bytes: &mut [u8; EC_SIGNATURE_SIZE]) {
-        bytes[.. 32].copy_from_slice(self.0.r());
-        bytes[32 ..].copy_from_slice(self.0.s());
+        bytes[.. 32].copy_from_slice(&self.r);
+        bytes[32 ..].copy_from_slice(&self.s);
     }
 
     fn to_der(&self) -> Vec<u8> {
-        let r = der_int(self.0.r());
-        let s = der_int(self.0.s());
+        let r = der_int(&self.r);
+        let s = der_int(&self.s);
         let mut der = Vec::with_capacity(72);
         der.extend([0x30, (2 + r.len() + 2 + s.len()) as u8]); // seq
         der.extend([0x02, r.len() as u8]); // int
@@ -103,8 +108,7 @@ impl ecdsa::Signature for Signature {
     }
 }
 
-fn der_int(x: &Int<P256>) -> Vec<u8> {
-    let x = x.as_slice();
+fn der_int(x: &[u8; 32]) -> Vec<u8> {
     let i = x.iter().position(|&x| x != 0).unwrap_or(x.len());
     let mut r = Vec::new();
     if x.get(i).is_none_or(|x| x & 0x80 != 0) {
@@ -122,7 +126,7 @@ mod tests {
     fn der_int_ok() {
         #[track_caller]
         fn test(x: &[u8; 32], r: &[u8]) {
-            assert_eq!(der_int(x.into()), r);
+            assert_eq!(der_int(x), r);
         }
         test(&[0; 32], &[0]);
         test(b"\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x01", &[1]);
