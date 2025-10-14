a3bb9dc170d42be82cc0675f5c535b5348b9711f
Julien Cretin <cretin@google.com>
2025-10-14 17:10:40 +0200

    Use binary protocol for fingerprint example (#945)

---
+++ examples/rust/fingerprint/host/src/main.rs
@@ -0,0 +1,201 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#![feature(exit_status_error)]
+
+use std::io::Write;
+use std::time::Duration;
+
+use anyhow::{Result, bail};
+use clap::Parser;
+use common::{Request, Response};
+use data_encoding::HEXLOWER_PERMISSIVE as HEX;
+use wasefire_cli_tools::action::ConnectionOptions;
+use wasefire_error::Error;
+use wasefire_protocol::applet::{self, AppletId};
+use wasefire_protocol::{self as service, Connection, ConnectionExt as _};
+use wasefire_wire::{Wire, Yoke};
+
+#[derive(Parser)]
+struct Flags {
+    #[command(flatten)]
+    options: ConnectionOptions,
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(clap::Subcommand)]
+enum Command {
+    /// Captures an image.
+    Capture,
+
+    /// Enrolls a finger and prints its template.
+    Enroll,
+
+    /// Identifies a finger.
+    Identify {
+        /// Template to identify against (all templates otherwise).
+        id: Option<String>,
+    },
+
+    /// Deletes a template.
+    Delete {
+        /// Template to delete (all templates otherwise).
+        id: Option<String>,
+    },
+
+    /// List all templates.
+    List,
+}
+
+macro_rules! call {
+    ($x:ident($c:expr): $i:tt -> $o:tt) => {
+        call!(1, $c, $x, $i, $o)
+    };
+    (1, $c:expr, $x:ident, (), $o:tt) => {
+        call!(2, $c, $x, Request::$x, $o)
+    };
+    (1, $c:expr, $x:ident, ($i:expr), $o:tt) => {
+        call!(2, $c, $x, Request::$x($i), $o)
+    };
+    (2, $c:expr, $x:ident, $i:expr, ()) => {
+        call!(3, $c, $x, $i, |x| match x {
+            Response::$x => Ok(()),
+            _ => Err(x),
+        })
+    };
+    (2, $c:expr, $x:ident, $i:expr, (_)) => {
+        call!(3, $c, $x, $i, |x| match x {
+            Response::$x(x) => Ok(x),
+            _ => Err(x),
+        })
+    };
+    (3, $c:expr, $x:ident, $i:expr, $o:expr) => {
+        call($c, stringify!($x), &$i, $o).await?
+    };
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let flags = Flags::parse();
+    let mut connection = flags.options.connect().await?;
+    match flags.command {
+        Command::Capture => {
+            call!(CaptureStart(&mut connection): () -> ()).get();
+            let mut stdout = std::io::stdout().lock();
+            write!(stdout, "Waiting for touch").unwrap();
+            let width = loop {
+                let width = call!(CaptureDone(&mut connection): () -> (_));
+                match width.try_map(|x| x.ok_or(())) {
+                    Ok(width) => break *width.get(),
+                    Err(()) => tokio::time::sleep(Duration::from_millis(300)).await,
+                }
+                write!(stdout, ".").unwrap();
+                stdout.flush().unwrap();
+            };
+            drop(stdout);
+            println!("\nSaved as image.raw and image.png");
+            let data = call_raw(&mut connection, &Request::CaptureImage).await?;
+            tokio::fs::write("image.raw", data.get()).await?;
+            let height = data.get().len() / width;
+            let size = format!("{width}x{height}");
+            let mut convert = tokio::process::Command::new("convert");
+            convert.args(["-size", &size, "-depth", "8", "gray:image.raw", "image.png"]);
+            convert.status().await?.exit_ok()?;
+        }
+        Command::Enroll => {
+            call!(EnrollStart(&mut connection): () -> ()).get();
+            let mut stdout = std::io::stdout().lock();
+            loop {
+                match call!(EnrollDone(&mut connection): () -> (_)).try_map(|x| x) {
+                    Ok(id) => {
+                        drop(stdout);
+                        println!(
+                            "\r\x1b[KEnroll finger at template id {}",
+                            HEX.encode_display(id.get())
+                        );
+                        break;
+                    }
+                    Err((detected, remaining)) => {
+                        let percent = detected * 100 / (detected + remaining.unwrap_or(1));
+                        write!(stdout, "\r\x1b[KWaiting for touch ({percent}%).").unwrap();
+                        stdout.flush().unwrap();
+                        tokio::time::sleep(Duration::from_millis(300)).await;
+                    }
+                }
+            }
+        }
+        Command::Identify { id } => {
+            let id = id.map(|x| HEX.decode(x.as_bytes())).transpose()?;
+            call!(IdentifyStart(&mut connection): (id) -> ()).get();
+            let mut stdout = std::io::stdout().lock();
+            write!(stdout, "Waiting for touch").unwrap();
+            let result = loop {
+                let result = call!(IdentifyDone(&mut connection): () -> (_));
+                match result.try_map(|x| x.ok_or(())) {
+                    Ok(result) => break result,
+                    Err(()) => tokio::time::sleep(Duration::from_millis(300)).await,
+                }
+                write!(stdout, ".").unwrap();
+                stdout.flush().unwrap();
+            };
+            drop(stdout);
+            println!();
+            match result.try_map(|x| x.ok_or(())) {
+                Ok(id) => println!("Matched template id {}", HEX.encode_display(id.get())),
+                Err(()) => println!("No match"),
+            }
+        }
+        Command::Delete { id } => {
+            let id = id.map(|x| HEX.decode(x.as_bytes())).transpose()?;
+            call!(Delete(&mut connection): (id) -> ()).get();
+            println!("Deleted");
+        }
+        Command::List => {
+            let ids = call!(List(&mut connection): () -> (_));
+            println!("There are {} template ids:", ids.get().len());
+            for id in ids.get() {
+                println!("- {}", HEX.encode_display(id));
+            }
+        }
+    }
+    Ok(())
+}
+
+async fn call<T: Wire<'static>>(
+    connection: &mut dyn Connection, name: &str, request: &Request,
+    extract: impl FnOnce(Response) -> Result<T, Response>,
+) -> Result<Yoke<T>> {
+    let response = call_raw(connection, request).await?;
+    let response = response.try_map(|x| wasefire_wire::decode::<Result<Response, Error>>(x)?)?;
+    match response.try_map(extract) {
+        Ok(x) => Ok(x),
+        Err(x) => bail!("unexpected response {x:?} for {name}"),
+    }
+}
+
+async fn call_raw(
+    connection: &mut dyn Connection, request: &Request,
+) -> Result<Yoke<&'static [u8]>> {
+    let request = wasefire_wire::encode(request)?;
+    let request = applet::Request { applet_id: AppletId, request: &request };
+    connection.call::<service::AppletRequest>(request).await?.get();
+    loop {
+        let response = connection.call::<service::AppletResponse>(AppletId).await?;
+        if let Ok(response) = response.try_map(|x| x.ok_or(())) {
+            break Ok(response);
+        }
+        tokio::time::sleep(Duration::from_millis(300)).await;
+    }
+}
--- examples/rust/fingerprint/src/lib.rs
+++ examples/rust/fingerprint/src/lib.rs
@@ -14,197 +14,114 @@
 
 //! Demonstrates simple fingerprint usage.
 //!
-//! The applet uses the platform protocol applet RPC system for its interface. It is a simple
-//! text-based interface and describes its usage when receiving "help" as a request:
-//!
-//!     ( echo help; cat ) | wasefire applet-rpc --repl
+//! The applet uses the platform protocol applet RPC system for its interface. It can be used with
+//! the companion `host` binary.
 
 #![no_std]
-#![feature(try_blocks)]
 wasefire::applet!();
 
-use alloc::format;
-use alloc::string::{String, ToString};
 use alloc::vec::Vec;
-use core::str::FromStr;
-use core::time::Duration;
 
-use data_encoding::HEXLOWER_PERMISSIVE as HEX;
+use common::{Request, Response};
 use wasefire::error::Code;
 use wasefire::fingerprint::matcher::{
     self, Enroll, EnrollProgress, Identify, IdentifyResult, delete_template, list_templates,
 };
 use wasefire::fingerprint::sensor::{self, Capture, Image};
-use wasefire::timer::Timeout;
+use wasefire::sync::Mutex;
 
 fn main() {
     rpc::Listener::new(&platform::protocol::RpcProtocol, handler).leak();
 }
 
 fn handler(request: Vec<u8>) -> Vec<u8> {
-    // Parse and process the request.
-    let result: Result<String, String> = try {
-        let request = String::from_utf8(request).map_err(|_| "Request is not UTF-8")?;
-        Command::parse(&request)?.process()?
-    };
-    // Format output including error and next prompt.
-    let mut output = result.unwrap_or_else(|error| format!("Error: {error}"));
-    output.push_str("\n> ");
-    output.into_bytes()
-}
-
-enum Command {
-    Help,
-    Capture,
-    Enroll,
-    Identify { id: Option<TemplateId> },
-    Delete { id: Option<TemplateId> },
-    List,
-}
-
-impl Command {
-    fn parse(input: &str) -> Result<Self, String> {
-        Ok(match *input.split_whitespace().collect::<Vec<_>>().as_slice() {
-            [] | ["help"] => Command::Help,
-            ["capture"] => Command::Capture,
-            ["enroll"] => Command::Enroll,
-            ["identify"] => Command::Identify { id: None },
-            ["identify", id] => Command::Identify { id: Some(TemplateId::parse(id)?) },
-            ["delete"] => Command::Delete { id: None },
-            ["delete", id] => Command::Delete { id: Some(TemplateId::parse(id)?) },
-            ["list"] => Command::List,
-            [command, ..] => return Err(format!("Invalid command {command:?}")),
-        })
+    match handler_(&request) {
+        Ok(Response::CaptureImage(data)) => data,
+        x => wasefire_wire::encode(&x).unwrap().into(),
     }
+}
 
-    fn process(&self) -> Result<String, String> {
-        match self {
-            Command::Help => Ok("\
-Usage: capture
-Usage: enroll
-Usage: identify [<template_id>]
-Usage: delete [<template_id>]
-Usage: list"
-                .to_string()),
-            Command::Capture if sensor::is_supported() => match capture() {
-                Ok(image) => {
-                    use core::fmt::Write as _;
-                    let width = image.width;
-                    let height = image.data.len() / image.width;
-                    let w = core::cmp::min(16, width);
-                    let h = core::cmp::min(8, height);
-                    let mut output = String::new();
-                    for i in 0 .. h {
-                        writeln!(output, "{:02x?}", &image.data[i * width ..][.. w]).unwrap();
-                    }
-                    write!(output, "Size: {width}x{height} ({w}x{h} displayed)").unwrap();
-                    Ok(output)
-                }
-                Err(error) => Err(format!("{error}")),
-            },
-            Command::Enroll if matcher::is_supported() => match enroll() {
-                Ok(id) => Ok(format!("Template ID: {id}")),
-                Err(error) => Err(format!("{error}")),
-            },
-            Command::Identify { id } if matcher::is_supported() => match identify(id) {
-                Ok(None) => Ok("No match".to_string()),
-                Ok(Some(id)) => Ok(format!("Matched {id}")),
-                Err(error) => Err(format!("{error}")),
-            },
-            Command::Delete { id } if matcher::is_supported() => match delete(id) {
-                Ok(()) => Ok("Done".to_string()),
-                Err(error) => Err(format!("{error}")),
-            },
-            Command::List if matcher::is_supported() => match list_templates() {
-                Ok(templates) => {
-                    use core::fmt::Write as _;
-                    let mut list = String::new();
-                    write!(list, "There are {} enrolled fingers.", templates.len()).unwrap();
-                    for template in templates.iter() {
-                        write!(list, "\n- {}", HEX.encode(template)).unwrap();
-                    }
-                    Ok(list)
-                }
-                Err(error) => Err(format!("{error}")),
-            },
-            _ => Err("Unsupported command".to_string()),
+fn handler_(request: &[u8]) -> Result<Response, Error> {
+    let mut state = STATE.lock();
+    match wasefire_wire::decode::<Request>(request)? {
+        Request::CaptureStart if sensor::is_supported() => {
+            *state = State::Capture(Capture::new()?);
+            Ok(Response::CaptureStart)
         }
-    }
-}
-
-struct TemplateId(Vec<u8>);
-
-impl FromStr for TemplateId {
-    type Err = data_encoding::DecodeError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        HEX.decode(s.as_bytes()).map(TemplateId)
-    }
-}
-
-impl core::fmt::Display for TemplateId {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        f.write_str(&HEX.encode(&self.0))
-    }
-}
-
-impl TemplateId {
-    fn parse(id: &str) -> Result<Self, String> {
-        id.parse().map_err(|_| "Failed to parse template id".to_string())
-    }
-}
-
-fn capture() -> Result<Image, Error> {
-    let capture = Capture::new()?;
-    let timeout = Timeout::new(Duration::from_secs(10));
-    scheduling::wait_until(|| capture.is_done() || timeout.is_over());
-    if timeout.is_over() {
-        debug!("Timed out.");
-        capture.abort()?;
-        Err(Error::user(Code::NotEnough))
-    } else {
-        capture.result()
-    }
-}
-
-fn enroll() -> Result<TemplateId, Error> {
-    let enroll = Enroll::new()?;
-    let timeout = Timeout::new(Duration::from_secs(60));
-    scheduling::wait_until(|| match enroll.progress() {
-        None => true,
-        Some(_) if timeout.is_over() => true,
-        Some(EnrollProgress { detected, remaining }) => {
-            let percent = detected * 100 / (detected + remaining.unwrap_or(1));
-            debug!("Enrollment progress: {percent}%.");
-            false
+        Request::CaptureStart => Err(Error::world(Code::NotImplemented)),
+        Request::CaptureDone => {
+            let State::Capture(capture) = &mut *state else {
+                return Err(Error::user(Code::InvalidState));
+            };
+            if !capture.is_done() {
+                return Ok(Response::CaptureDone(None));
+            }
+            let State::Capture(capture) = core::mem::take(&mut *state) else { unreachable!() };
+            let Image { data, width } = capture.result()?;
+            *state = State::Image(data.into());
+            Ok(Response::CaptureDone(Some(width)))
         }
-    });
-    if timeout.is_over() {
-        debug!("Timed out.");
-        enroll.abort()?;
-        Err(Error::user(Code::NotEnough))
-    } else {
-        let id = enroll.result()?;
-        Ok(TemplateId(id.into_vec()))
+        Request::CaptureImage => {
+            if !matches!(*state, State::Image(_)) {
+                return Err(Error::user(Code::InvalidState));
+            }
+            let State::Image(data) = core::mem::take(&mut *state) else { unreachable!() };
+            Ok(Response::CaptureImage(data))
+        }
+        Request::EnrollStart if matcher::is_supported() => {
+            *state = State::Enroll(Enroll::new()?);
+            Ok(Response::EnrollStart)
+        }
+        Request::EnrollStart => Err(Error::world(Code::NotImplemented)),
+        Request::EnrollDone => {
+            let State::Enroll(enroll) = &mut *state else {
+                return Err(Error::user(Code::InvalidState));
+            };
+            if let Some(EnrollProgress { detected, remaining }) = enroll.progress() {
+                return Ok(Response::EnrollDone(Err((detected, remaining))));
+            }
+            let State::Enroll(enroll) = core::mem::take(&mut *state) else { unreachable!() };
+            Ok(Response::EnrollDone(Ok(enroll.result()?.into())))
+        }
+        Request::IdentifyStart(id) if matcher::is_supported() => {
+            *state = State::Identify(Identify::new(id.as_deref())?);
+            Ok(Response::IdentifyStart)
+        }
+        Request::IdentifyStart(_) => Err(Error::world(Code::NotImplemented)),
+        Request::IdentifyDone => {
+            let State::Identify(identify) = &mut *state else {
+                return Err(Error::user(Code::InvalidState));
+            };
+            if !identify.is_done() {
+                return Ok(Response::IdentifyDone(None));
+            }
+            let State::Identify(identify) = core::mem::take(&mut *state) else { unreachable!() };
+            let result = match identify.result()? {
+                IdentifyResult::NoMatch => None,
+                IdentifyResult::Match { template } => Some(template.into()),
+            };
+            Ok(Response::IdentifyDone(Some(result)))
+        }
+        Request::Delete(id) if matcher::is_supported() => {
+            delete_template(id.as_deref())?;
+            Ok(Response::Delete)
+        }
+        Request::Delete(_) => Err(Error::world(Code::NotImplemented)),
+        Request::List if matcher::is_supported() => {
+            Ok(Response::List(list_templates()?.iter().map(|x| x.to_vec()).collect()))
+        }
+        Request::List => Err(Error::world(Code::NotImplemented)),
     }
 }
 
-fn identify(id: &Option<TemplateId>) -> Result<Option<TemplateId>, Error> {
-    let identify = Identify::new(id.as_ref().map(|x| x.0.as_slice()))?;
-    let timeout = Timeout::new(Duration::from_secs(10));
-    scheduling::wait_until(|| identify.is_done() || timeout.is_over());
-    if timeout.is_over() {
-        debug!("Timed out.");
-        identify.abort()?;
-        Err(Error::user(Code::NotEnough))
-    } else {
-        Ok(match identify.result()? {
-            IdentifyResult::NoMatch => None,
-            IdentifyResult::Match { template } => Some(TemplateId(template.into_vec())),
-        })
-    }
+#[derive(Default)]
+enum State {
+    #[default]
+    Idle,
+    Capture(Capture),
+    Image(Vec<u8>),
+    Enroll(Enroll),
+    Identify(Identify),
 }
 
-fn delete(id: &Option<TemplateId>) -> Result<(), Error> {
-    delete_template(id.as_ref().map(|x| x.0.as_slice()))
-}
+static STATE: Mutex<State> = Mutex::new(State::Idle);
