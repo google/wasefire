5d8caf311f159d5181ed6f5f610c9e9cc1cc9a35
Julien Cretin <cretin@google.com>
2026-02-10 11:42:21 +0100

    Make sure setup is called during release

--- scripts/artifacts.sh
+++ scripts/artifacts.sh
@@ -60,6 +60,7 @@ i "Build web-client once for all supported targets"
 
 i "Build the CLI for each supported target"
 TARGETS='
+i686-unknown-linux-gnu
 x86_64-unknown-linux-gnu
 '
 for target in $TARGETS; do
