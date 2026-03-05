dbf7e8f5503332289a12b044c287e7317b96550f
Julien Cretin <cretin@google.com>
2026-03-05 13:31:16 +0000

    7b3e8f0616392595bc6b98004ca2566f085dc061

---
+++ applet/prelude/button2.rs
@@ -0,0 +1,73 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Demonstrates simple LED and button interaction.
+//!
+//! The board must possess at least one LED and one button. The applet maintains a dynamic mapping
+//! from buttons to LEDs so that every button may control any LED over time. Initially, all buttons
+//! are associated to the first LED. Each time a button is pressed or released, its associated LED
+//! is toggled. After a button is released, it becomes associated with the next LED (wrapping back
+//! to the first LED after the last LED).
+
+//{ ANCHOR: all
+#![no_std]
+wasefire::applet!();
+
+use core::cell::Cell;
+
+fn main() -> Result<(), Error> {
+    //{ ANCHOR: setup
+    // Make sure there is at least one button.
+    let num_buttons = button::count();
+    assert!(num_buttons > 0, "Board has no buttons.");
+
+    // Make sure there is at least one LED.
+    let num_leds = led::count();
+    assert!(num_leds > 0, "Board has no LEDs.");
+
+    // For each button on the board.
+    for button_index in 0 .. num_buttons {
+    //} ANCHOR_END: setup
+        //{ ANCHOR: state
+        // We create the state containing the LED to which this button maps to.
+        let led_state = Cell::new(0);
+        //} ANCHOR_END: state
+
+        //{ ANCHOR: handler
+        // We define the button handler and move the state to there.
+        let handler = move |button_state| {
+        //} ANCHOR_END: handler
+            //{ ANCHOR: toggle
+            // We toggle the LED.
+            let led_index = led_state.get();
+            led::set(led_index, !led::get(led_index));
+            //} ANCHOR_END: toggle
+
+            //{ ANCHOR: update
+            // If the button is released, we point it to the next LED.
+            if matches!(button_state, button::Released) {
+                led_state.set((led_index + 1) % num_leds);
+            }
+            //} ANCHOR_END: update
+        };
+
+        //{ ANCHOR: listener
+        // We indefinitely listen by creating and leaking a listener.
+        button::Listener::new(button_index, handler)?.leak();
+        //} ANCHOR_END: listener
+    }
+
+    Ok(())
+}
+//} ANCHOR_END: all
---
+++ applet/prelude/timer.rs
@@ -0,0 +1,113 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Demonstrates simple timer usage to cancel button press.
+//!
+//! The applet relies on LEDs, buttons, and timers. The applet uses the first button and first LED.
+//! When the button is pressed, the LED turns on and a 1 second timer starts. If the timer runs out
+//! before the button is released, then the LED turns off and releasing the button will do nothing.
+//! If the button is released before the timer runs out, then the LED will blink until next button
+//! press.
+
+//{ ANCHOR: all
+#![no_std]
+wasefire::applet!();
+
+use alloc::rc::Rc;
+use core::cell::Cell;
+use core::time::Duration;
+
+fn main() -> Result<(), Error> {
+    assert!(button::count() > 0, "Board has no buttons.");
+    assert!(led::count() > 0, "Board has no LEDs.");
+
+    //{ ANCHOR: blinking
+    // We define a shared state to decide whether we must blink.
+    let blinking = Rc::new(Cell::new(false));
+    //} ANCHOR_END: blinking
+
+    //{ ANCHOR: blink
+    // Allocate a timer for blinking the LED.
+    let blink = timer::Timer::new({
+        // Move a clone of the state to the callback.
+        let blinking = blinking.clone();
+        move || {
+            // Toggle the LED if blinking.
+            if blinking.get() {
+                led::set(0, !led::get(0));
+            }
+        }
+    });
+    //} ANCHOR_END: blink
+
+    //{ ANCHOR: loop
+    loop {
+    //} ANCHOR_END: loop
+        //{ ANCHOR: button
+        // Setup button listeners.
+        let pressed = Rc::new(Cell::new(false));
+        let released = Rc::new(Cell::new(false));
+        let _button = button::Listener::new(0, {
+            let pressed = pressed.clone();
+            let released = released.clone();
+            move |state| match state {
+                button::Pressed => pressed.set(true),
+                button::Released if pressed.get() => released.set(true),
+                button::Released => (),
+            }
+        })?;
+        //} ANCHOR_END: button
+
+        //{ ANCHOR: pressed
+        // Wait for the button to be pressed.
+        scheduling::wait_until(|| pressed.get());
+        //} ANCHOR_END: pressed
+
+        //{ ANCHOR: led_on
+        // Turn the LED on.
+        blink.stop();
+        blinking.set(false);
+        led::set(0, led::On);
+        //} ANCHOR_END: led_on
+
+        //{ ANCHOR: timeout
+        // Start the timeout to decide between short and long press.
+        let timed_out = Rc::new(Cell::new(false));
+        let timer = timer::Timer::new({
+            let timed_out = timed_out.clone();
+            move || timed_out.set(true)
+        });
+        timer.start(timer::Oneshot, Duration::from_secs(1));
+        //} ANCHOR_END: timeout
+
+        //{ ANCHOR: released
+        // Wait for the button to be released or the timeout to fire.
+        scheduling::wait_until(|| released.get() || timed_out.get());
+        //} ANCHOR_END: released
+
+        //{ ANCHOR: led_off
+        // Turn the LED off.
+        led::set(0, led::Off);
+        //} ANCHOR_END: led_off
+
+        //{ ANCHOR: short
+        // Start blinking if short press.
+        if !timed_out.get() {
+            blinking.set(true);
+            blink.start(timer::Periodic, Duration::from_millis(200));
+        }
+        //} ANCHOR_END: short
+    }
+}
+//} ANCHOR_END: all
