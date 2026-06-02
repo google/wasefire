6484a0fd3bd60a9621d63d46dd5833062cab5fd8
Julien Cretin <cretin@google.com>
2026-06-02 09:55:48 +0200

    Install rustup from submodule again
    This reverts #1074.
    
    This is not done for hermetic builds but for source integrity.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -44,10 +44,11 @@ ensure bin curl
 ensure bin pkg-config
 
 if ! has bin rustup; then
+  x git submodule update --init third_party/rust-lang/rustup
   if [ -n "$WASEFIRE_YES" ]; then
-    RUSTUP_ARGS='-s -- -y --default-toolchain=none --profile=minimal --no-modify-path'
+    RUSTUP_ARGS='-y --default-toolchain=none --profile=minimal --no-modify-path'
   fi
-  x curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | x sh $RUSTUP_ARGS
+  x ./third_party/rust-lang/rustup/rustup-init.sh $RUSTUP_ARGS
 fi
 
 # Transitive dependencies of xtask.
