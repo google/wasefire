e70d13e86b6bb8995e13e917d745d7826a676bf6
Julien Cretin <cretin@google.com>
2026-08-06 11:48:01 +0200

    Update OpenTitan to match prototype

--- crates/runner-opentitan/src/board/led.rs
+++ crates/runner-opentitan/src/board/led.rs
@@ -20,25 +20,38 @@ use wasefire_error::Error;
 pub enum Impl {}
 
 impl Support<usize> for Impl {
-    const SUPPORT: usize = 1;
+    const SUPPORT: usize = 4;
 }
 
 impl Api for Impl {
     fn get(led: Id<Self>) -> Result<bool, Error> {
-        Ok((GPIO.direct_out().read_raw() & 1 << *led) != 0)
+        Ok((GPIO.direct_out().read_raw() & 1 << *led) == 0)
     }
 
     fn set(led: Id<Self>, on: bool) -> Result<(), Error> {
         let mask = 1 << *led;
-        GPIO.masked_out_lower().reset().mask(mask).data(if on { mask } else { 0 }).reg.write();
+        GPIO.masked_out_lower().reset().mask(mask).data(if on { 0 } else { mask }).reg.write();
         Ok(())
     }
 }
 
 pub fn init() {
-    // TODO: Generate top_earlgrey_pinmux_{mio_out,outsel}. This should be MioOutIor10 and
-    // GpioGpio0.
-    PINMUX_AON.mio_outsel(43).reset().out(3).reg.write();
+    // TODO: Generate top_earlgrey_pinmux_{mio_out,outsel}. We use the following constants:
+    // - MioOutIob4 = 13
+    // - MioOutIob7 = 16
+    // - MioOutIob9 = 18
+    // - MioOutIob11 = 20
+    // - GpioGpio0 = 3
+    // - GpioGpio1 = 4
+    // - GpioGpio2 = 5
+    // - GpioGpio3 = 6
+    for pad in [13, 16, 18, 20] {
+        PINMUX_AON.mio_pad_attr(pad).reset().input_disable(true).reg.write();
+    }
+    for (pad, gpio) in [(13, 3), (16, 4), (18, 5), (20, 6)] {
+        PINMUX_AON.mio_outsel(pad).reset().out(gpio).reg.write();
+    }
     let mask = (1 << Impl::SUPPORT) - 1;
     GPIO.masked_oe_lower().reset().mask(mask).data(mask).reg.write();
+    GPIO.masked_out_lower().reset().mask(mask).data(mask).reg.write();
 }
